1ea6062c7953b2ead766bd07fb0c7958b832fdf0
HUS1904 <s235110@dtu.dk>
2024-03-22 21:42:39 +0100

    Added a main menu

--- Yukon/Main.c
+++ Yukon/Main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-char lastCommand[];
+char lastCommand[] = input;
 char message[];
 char input[];
 
 void run();
 void printBoard();
+void mainMenu();
 
 int main(){
     run();
@@ -15,10 +16,14 @@ void printBoard(){
     printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\t\n");
 
     //More to be implemented.
+}
 
+void mainMenu(){
     printf("\nLAST Command: %c", lastCommand);
     printf("\nMessage: %c", message);
     printf("\nINPUT > ");
+    scanf("%c", input);
+
 }
 
 void run(){
