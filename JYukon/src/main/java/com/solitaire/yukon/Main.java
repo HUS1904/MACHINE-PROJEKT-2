ad214b5fe663326ce8401e86745f1bdc9d7ff970
Andrei Enache <andreienake@gmail.com>
2024-04-30 00:56:52 +0200

    Shortened main file

--- JYukon/src/main/java/com/solitaire/yukon/Main.java
+++ JYukon/src/main/java/com/solitaire/yukon/Main.java
@@ -1,7 +1,6 @@
 package com.solitaire.yukon;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,73 +10,51 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 
-public class HelloApplication extends Application {
+public class Main extends Application {
    static TextField text;
     static DatagramSocket socket;
     static InetAddress serverAddress; // Change this to the actual server address
     static int serverPort = 8080;
-    static  VBox root;
+    static VBox root;
     static Label status;
-    static String[][] colsAndFound = new String[11][];
+    static ArrayList<ArrayList<String>> colsAndFound = new ArrayList<>();
     static GridPane cardGround = new GridPane();
     static Button button;
     @Override
     public void start(Stage stage) throws IOException {
-
-
-
-         serverAddress = InetAddress.getByName("127.0.0.1"); // Change this to the actual server address
+        serverAddress = InetAddress.getByName("127.0.0.1"); // Change this to the actual server address
         int serverPort = 8080;
 
         // Create UDP socket
-         socket = new DatagramSocket();
-
-
-
+        socket = new DatagramSocket();
 
         root = new VBox();
         root.setSpacing(0);
         root.setPadding(new Insets(20, 20, 20, 20));
 
-
-
-
-
-
-
-
         button = new Button("send");
         button.setPadding(new Insets(10, 10, 10, 10));
         button.setOnAction(e -> onButtonPressed());
 
-
-
-        //
-
-
-        //
         status = new Label("awaiting for input: ");
-
-
-        //
-
 
         text = new TextField();
         text.setPromptText("Enter Command");
@@ -112,26 +89,13 @@ public class HelloApplication extends Application {
                 if(message.equals("LD")){
                     status.setText("Deck Loaded");
                 } else {
-                    int counter = 0;
                     String relativePath = "state.txt"; // Example relative path
                     File file = new File(relativePath);
-                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                        String line;
 
-                        while ((line = reader.readLine()) != null && counter < 7) {
-                            System.out.println(line);
+                    colsAndFound = Files.lines(Path.of(file.getPath()))
+                                        .map(x-> new ArrayList<>(Arrays.asList(x.split(" "))))
+                                        .collect(Collectors.toCollection(ArrayList::new));
 
-                            // Split the line into an array of strings based on spaces
-                            String[] split = line.split(" ");
-
-                            // Assign the split array to the corresponding row in colsAndFound
-                            colsAndFound[counter] = split;
-
-                            counter++;
-                        }
-                    } catch (IOException e) {
-                        System.err.println("Error occurred while reading the file: " + e.getMessage());
-                    }
                     updateView();
                 }
 
@@ -139,7 +103,7 @@ public class HelloApplication extends Application {
 
                 status.setText("Error: " + response);
             }
-        } catch(IOException e) {
+        } catch(IOException _) {
 
         }
 
@@ -150,29 +114,18 @@ public class HelloApplication extends Application {
         GridPane grid = new GridPane();
         grid.setHgap(30); // Set horizontal gap between columns
         grid.setVgap(20); // Set vertical gap between rows
-        VBox[] vboxes = new VBox[7];
-        double verticalOffset = 1000 / 13;
+        ArrayList<VBox> vboxes = new ArrayList<>();
+        double verticalOffset = 1000.0 / 13.0;
 
-        // Initialize VBox containers and populate them with Card objects
-        for (int i = 0; i < 7; i++) {
-            String[] cards = colsAndFound[i];
-            vboxes[i] = new VBox();
-
-            // Populate each VBox with Card objects
-            for (int j = 0; j < cards.length; j++) {
-                String name = cards[j];
-                Card card = new Card("turned");
-                vboxes[i].getChildren().add(card);
+        colsAndFound.forEach(x -> {
+            VBox vbox = new VBox();
+            x.forEach(y -> {
+                Card card = new Card("turned" /*To be replaced with "y" when additional assets are added*/);
                 card.setTranslateY(50);
-            }
-
-
-
-
-            // Add the populated VBox to the GridPane
-            grid.add(vboxes[i], i, 0); // Add VBox to the corresponding column (i)
-        }
-
+                vbox.getChildren().add(card);
+            });
+            grid.add(vbox, colsAndFound.indexOf(x),0);
+        });
         // Add the GridPane and other nodes back to the root VBox
         root.getChildren().addAll(grid, button, text, status);
     }
@@ -180,8 +133,7 @@ public class HelloApplication extends Application {
         launch();
     }
 
-  class Card extends Pane {
-
+  static class Card extends Pane {
         private static final double WIDTH = 100;
         private static final double HEIGHT = 150;
         Image image ;
@@ -190,7 +142,7 @@ public class HelloApplication extends Application {
             setPrefSize(WIDTH, HEIGHT);
             Rectangle cardShape = new Rectangle(WIDTH, HEIGHT, Color.WHITE);
             cardShape.setStroke(Color.BLACK);
-            image = new Image(getClass().getResourceAsStream("/" + name + ".png"));
+            image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/" + name + ".png")));
 
             ImageView imageView = new ImageView(image);
 
